5186dc8f852eb445fd08e0f3c25c01fc176955c8
Gokuu <goktug_ctn_66@hotmail.com>
2023-02-11 22:55:24 +0300

    Main menu Buttons And Play menu...

---
+++ Precious/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public void PlayMenu()
+    {
+        SceneManager.LoadScene("PlayMenu");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void FirstMap()
+    {
+        SceneManager.LoadScene("FirstMap");
+    }
+    public void SecondMap()
+    {
+        SceneManager.LoadScene("SecondMap");
+    }
+    public void ThirdMap()
+    {
+        SceneManager.LoadScene("ThirdMap");
+    }
+    public void MapFour()
+    {
+        SceneManager.LoadScene("4thMap");
+    }
+    public void MapFive()
+    {
+        SceneManager.LoadScene("5thMap");
+    }
+    public void MapSix()
+    {
+        SceneManager.LoadScene("6thMap");
+    }
+    public void MapSeven()
+    {
+        SceneManager.LoadScene("7thMap");
+    }
+    public void MapEight()
+    {
+        SceneManager.LoadScene("8thMap");
+    }
+}
